11a231b3e218e7de53b52b4afa20f535a81e02b3
deepu519 <107624626+deepu519@users.noreply.github.com>
2022-06-16 23:46:12 +0530

    Add files via upload

---
+++ src/main/java/deepAto/MobileAuto/App.java
@@ -0,0 +1,13 @@
+package deepAto.MobileAuto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
